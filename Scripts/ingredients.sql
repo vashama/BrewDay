a6e910354076cda808a63fe799e48f055d993218
run-dong-zhu <rundong.zhu@asu.edu>
2018-01-23 20:04:04 -0700

    Create ingredients.sql

---
+++ Scripts/ingredients.sql
@@ -0,0 +1,13 @@
+CREATE TABLE ingredients (
+    id INT NOT NULL,
+    name VARCHAR (255) NOT NULL,
+    type VARCHAR (255),
+    capacity DOUBLE precision,
+    PRIMARY KEY (ID)
+);
+
+INSERT INTO ingredients VALUES 
+(0001, 'hops', 5.3), 
+(0002, 'wheat', 11), 
+(0003, 'water', 500), 
+(0004, 'yeast', 3.1)
