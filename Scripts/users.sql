f08b3046ddd99b23060e4bb0f14a598722185fcb
run-dong-zhu <rundong.zhu@asu.edu>
2018-01-23 20:02:28 -0700

    Create users.sql

---
+++ Scripts/users.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    psword VARCHAR(255) NOT NULL,
+    PRIMARY KEY (ID)
+);
+
+
+INSERT INTO users VALUES (0001, 'Rundong', '001'), 
+(0002, 'Hari', '010'), (0003, 'Akshay', '011'), (0004, 'Varuni', '100'), 
+(0005, 'Sneha', '101'), (0006, 'Abhi', '110')
