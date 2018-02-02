09b9a63bfb582e0b69070edf6d6be6f6095d8329
hariiyerasu1729 <hari.iyer@asu.edu>
2018-02-02 16:43:07 -0700

    Login Parameters validation added
    The REST receiver here has a placeholder for the class that would have methods to perform operations. It has been tested against the client login page.

--- BrewDay/brewDayBackend/src/main/java/com/brewday/web/AnalyzerEndPoint.java
+++ BrewDay/brewDayBackend/src/main/java/com/brewday/web/AnalyzerEndPoint.java
@@ -13,17 +13,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/post")
 public class AnalyzerEndPoint {
-
+	String returnParameter = "success";
 	@RequestMapping(value = "/response", method = RequestMethod.POST)
 	@ResponseBody
-	public ArrayList<String> Submit(@RequestParam("query") String query) {\\
-		//Call user-defined functions
-		System.out.println("Recieved Query: " + query);
-		/*
-		for(String entry : resultCollection){
-	        System.out.println(entry + "\n");
-	    }
-		*/
-		return resultCollection;
+	public String Submit(@RequestParam("username") String username, @RequestParam("password") String password) {
+		//This method can have more arguments depending on the required functoinality
+		System.out.println("received username: " + username);
+		System.out.println("received password: " + password);
+		//Temporary placeholder for the class with Postgres database linking methods
+		//DBAccess dbAccess = new DBAccess()
+		//Testing connection
+		if(!username.equals("testUser@gmail.com") && !password.equals("testPass"))
+			returnParameter = "failure";
+		
+		return returnParameter;
 	}
 }
