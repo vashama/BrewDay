1c07aba7b34688d1b8f4bc32243559166e66334f
run-dong-zhu <rundong.zhu@asu.edu>
2018-02-08 20:30:20 -0700

    Update users.sql
    Update user model in database. Related with Task #23

--- Scripts/users.sql
+++ Scripts/users.sql
@@ -1,15 +1,16 @@
 CREATE TABLE users (
     id INT NOT NULL,
-    name VARCHAR(255) NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL
     psword VARCHAR(255) NOT NULL,
     PRIMARY KEY (ID)
 );
 
 
 INSERT INTO users VALUES 
-(0001, 'Rundong', '001'), 
-(0002, 'Hari', '010'), 
-(0003, 'Akshay', '011'), 
-(0004, 'Varuni', '100'), 
-(0005, 'Sneha', '101'), 
-(0006, 'Abhi', '110')
+(0001, 'Rundong', 'Zhu', '001'), 
+(0002, 'Hari', 'Iyer', '010'), 
+(0003, 'Akshay', 'Ak', '011'), 
+(0004, 'Varuni', 'Va', '100'), 
+(0005, 'Sneha', 'Sn', '101'), 
+(0006, 'Abhi', 'Ab', '110')
