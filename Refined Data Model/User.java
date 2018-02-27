118bc0f56d52125bf021998f6797c913c6197905
run-dong-zhu <rundong.zhu@asu.edu>
2018-02-27 15:48:01 -0700

    Refine User Data Model
    Task #98

---
+++ Refined Data Model/User.java
@@ -0,0 +1,50 @@
+package brew_day;
+
+public class User {
+	private int userId;
+	private String name;
+	private String email;
+	private String password;
+	private byte[] photo;
+	
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public byte[] getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
