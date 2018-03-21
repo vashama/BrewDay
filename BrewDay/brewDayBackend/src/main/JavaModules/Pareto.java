04967f5486499541299d1f270c81843332d275d8
hariiyerasu1729 <hari.iyer@asu.edu>
2018-03-21 12:01:28 -0700

    User Story #50, Task #51: Updated code to access brew data from Database
    The code has been modified to access database to generate output for Pareto optimization to suggest ideal brew recipe.

--- BrewDay/brewDayBackend/src/main/JavaModules/Pareto.java
+++ BrewDay/brewDayBackend/src/main/JavaModules/Pareto.java
@@ -1,54 +1,66 @@
 package com.brewdaybackend.javamodules;
 
+
 import java.util.*;
 import org.apache.commons.lang3.ArrayUtils;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Pareto {
 	
+	static ArrayList<Double> ingredientsArrayList = new ArrayList<Double>();
+	static ArrayList<ArrayList<Double>> nestedIngredientsArrayList = new ArrayList<ArrayList<Double>>();
+	static ArrayList<String> ingredients = new ArrayList<String>();
+	static ArrayList<String> recipes = new ArrayList<String>();
+	static ArrayList<String> brewEquipment = new ArrayList<String>();
+	static ArrayList<Double> availableIngredients = new ArrayList<Double>();
+	
 	//Constructor to connect to Postgres Database
-	public Pareto()
+	public Pareto() throws SQLException, ClassNotFoundException 
 	{
 	      Connection conn = null;
-	      try 
+	      Statement sqlStatement = null;
+          Class.forName("org.postgresql.Driver");
+          conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres");
+
+	      sqlStatement = conn.createStatement();
+	      ResultSet rs1 = sqlStatement.executeQuery( "SELECT * FROM ingredients;" );
+	      
+	      
+	      
+	      while(rs1.next())
 	      {
-	         Class.forName("org.postgresql.Driver");
-	         conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/testdb", "postgres", "postgres");
+	          ingredientsArrayList.add(rs1.getDouble("hops"));
+	          ingredientsArrayList.add(rs1.getDouble("yeast"));
+	          ingredientsArrayList.add(rs1.getDouble("sugar"));
+	          ingredientsArrayList.add(rs1.getDouble("water"));
+	          ingredientsArrayList.add(rs1.getDouble("grains"));
+		      nestedIngredientsArrayList.add((ArrayList<Double>) ingredientsArrayList.clone());
+		      ingredientsArrayList.clear();
 	      }
-	      catch (Exception e)
+	      
+	      ResultSet rs2 = sqlStatement.executeQuery( "SELECT * FROM availableingredients;" );
+	      while(rs2.next())
 	      {
-	         e.printStackTrace();
+		      
+		      availableIngredients.add(rs2.getDouble("hops"));
+		      availableIngredients.add(rs2.getDouble("yeast"));
+	          availableIngredients.add(rs2.getDouble("sugar"));
+	          availableIngredients.add(rs2.getDouble("water"));
+	          availableIngredients.add(rs2.getDouble("grains"));
+	      }
+	      ResultSet rs3 = sqlStatement.executeQuery( "SELECT * FROM recipelist;" );
+	      while(rs3.next())
+	      {
+	          
+	          recipes.add(rs3.getString("recipes"));
 	      }
 	}
 	
-	//All the data used below would be fetched from the homebrewers database
 	
-	//Ingredients List
-	static String[] ingredients = new String[] { "Hops", "Yeast", "Sugar", "Water", "Grains"};
-	
-	//Recipe List
-	static String[] recipes = new String[] { "Cauim", "Millet Beer", "Draught Beer", "Malt Beer", "Pilsner", "Amber Ale"};
-	
-	//Brewing Equipment List
-	static String[] brewEquipments = new String[] { "Grain Mill", "Water Filter", "Hop Socks"};
-			
-	//Recipe based ingredient requirements
-	static Double[][] recipeRequirements = new Double[][]
-	{ 
-	{ 3.3, 16.3, 14.7, 69.0, 14.5 },
-	{ 4.2, 17.5, 16.9, 72.1, 13.4 },
-	{ 5.7, 67.4, 12.3, 81.2, 18.8 },
-	{ 6.6, 23.6, 15.3, 67.7, 29.6 },
-	{ 8.5, 35.7, 28.6, 76.4, 20.2 },
-	{ 7.8, 24.1, 22.5, 64.3, 17.1 }
-	};
-	
-	//Data structure to store ordered ingredients based on the availability
-	static double optimalityMatrix[][] = new double[recipeRequirements.length][recipeRequirements[0].length];
-	
-	//Available ingredients with the homebrewer
-	static Double[] availableIngredients = new Double[] {3.6, 22.5, 20.0, 65.1, 20.1};
 	
 	
 	public static String whatShouldIBrewToday()
@@ -59,9 +71,9 @@ public class Pareto {
 		
 		for (double ingredientValue: availableIngredients) {
 			
-			for(Double[] individualRecipeRequirements: recipeRequirements) {
+			for(ArrayList<Double> individualRecipeRequirements: nestedIngredientsArrayList) {
 				
-				ingredientBuffer.add(individualRecipeRequirements[ArrayUtils.indexOf(availableIngredients, ingredientValue)]);
+				ingredientBuffer.add(individualRecipeRequirements.get(availableIngredients.indexOf(ingredientValue)));
 			}
 			
 			Collections.sort(ingredientBuffer);
@@ -74,7 +86,7 @@ public class Pareto {
 			        i++;
 			}
 			
-			ingredientBufferCollection.add((ArrayList) ingredientBuffer.clone());
+			ingredientBufferCollection.add((ArrayList<Double>) ingredientBuffer.clone());
 			ingredientBuffer.clear();
 
 
@@ -86,7 +98,7 @@ public class Pareto {
 			
 			
 			
-			for(int i = 0; i < recipeRequirements[0].length; i++)
+			for(int i = 0; i < nestedIngredientsArrayList.get(0).size() - 1; i++)
 			if(bufferBreakup.get(i) == 999.99) {
 				
 				sufficientIngredients[i] = 1;
@@ -97,14 +109,17 @@ public class Pareto {
 		
 		int bestRecipeIndex = Arrays.asList(sufficientIngredients).lastIndexOf(0);
 				
-		return recipes[bestRecipeIndex];
+		return recipes.get(bestRecipeIndex);
 	}
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 	
+		
 		String optimalRecipe;
 		
+		new Pareto();
+
 		optimalRecipe = whatShouldIBrewToday();
 		
 		System.out.print("You should brew " + optimalRecipe + " today!");
