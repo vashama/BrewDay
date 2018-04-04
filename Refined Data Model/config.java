8cbdf11e57439f3593c8c1fadf137be8165a92aa
run-dong-zhu <rundong.zhu@asu.edu>
2018-04-03 18:35:56 -0700

    Create config.java
    User Story #133, Task #134

---
+++ Refined Data Model/config.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class ReadProperties {
+    private static Properties props = null;
+
+    //Load static properties from the properties file
+    private static void load() {
+        try {
+            InputStream inputStream = ReadProperties.class.getResourceAsStream("/jdbc.config");
+
+            props = new Properties();
+            props.load(inputStream);
+
+            inputStream.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Go get the property we want
+    public static String getProperty(String property) {
+        if(props == null)
+            load();
+
+        return(props.getProperty(property));
+    }
+}
