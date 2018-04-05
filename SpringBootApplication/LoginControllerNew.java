c633dfe5cd5d764f5d390f23a1c304fa6497a268
Sneha Vidhyashekar <32696696+SnehaVshekar@users.noreply.github.com>
2018-04-04 21:19:32 -0700

    #169 Integrating signup page with the backend
    Integrating Signup to the controller.

--- SpringBootApplication/LoginControllerNew.java
+++ SpringBootApplication/LoginControllerNew.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpSession;
 
 @RestController
+@RequestMapping("/user")
 public class LoginController {
 
     @Autowired
