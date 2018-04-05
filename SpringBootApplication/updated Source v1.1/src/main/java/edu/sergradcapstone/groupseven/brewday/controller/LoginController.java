7a261eed854c8328638abec8e395a791e663d13f
Sneha Vidhyashekar <32696696+SnehaVshekar@users.noreply.github.com>
2018-04-04 21:40:18 -0700

    User Story #157 -- Task #160 Recipe Back-end
    Recipe has been integrated with the remaining back-end application.

---
+++ SpringBootApplication/updated Source v1.1/src/main/java/edu/sergradcapstone/groupseven/brewday/controller/LoginController.java
@@ -0,0 +1,74 @@
+package edu.sergradcapstone.groupseven.brewday.controller;
+
+
+import edu.sergradcapstone.groupseven.brewday.model.ResourceNotFoundException;
+import edu.sergradcapstone.groupseven.brewday.model.User;
+import edu.sergradcapstone.groupseven.brewday.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import javax.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("/user")
+
+public class LoginController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping("/login")
+    public ResponseEntity processLogin(HttpSession httpSession, @RequestParam("username") String username, @RequestParam("password") String password) {
+
+        System.out.println("entering login controller");
+        User user = userRepository.findOneByEmail(username);
+        if (user == null) {
+            System.out.println("checking if the database has this user to authenticate");
+
+            throw new ResourceNotFoundException("Username", "id", username);
+        }
+        if (user.getPassword().contentEquals(password)) {
+            System.out.println("password from form is "+password);
+            System.out.println("password from db is "+user.getPassword());
+
+
+            System.out.println("checking if the database has the password is correct");
+
+            httpSession.setAttribute("username", username);
+            return ResponseEntity.ok("success");
+        }
+        return ResponseEntity.status(401).build();
+    }
+
+    @RequestMapping("/logout")
+    public ResponseEntity processLogout(HttpSession httpSession){
+        httpSession.invalidate();
+        return ResponseEntity.ok(200);
+    }
+
+
+
+    @RequestMapping(value="/signup" ,method= RequestMethod.POST)
+    public ResponseEntity processSignUp(HttpSession httpSession, @PathParam("firstname") String firstname,
+                                        @PathParam("lastname") String lastname, @PathParam("password") String password,
+                                        @PathParam("email") String email)
+    {
+        User user = new User();
+
+        if(userRepository.findOneByEmail(email) != null){
+            return ResponseEntity.status(400).body("Username already exists");
+        }
+
+        user.setFirstname(firstname);
+        user.setLastname(lastname);
+        user.setEmail(email);
+        user.setPassword(password);
+        userRepository.save(user);
+        return ResponseEntity.ok(201);
+    }
+}
---
+++ SpringBootApplication/updated Source v1.1/src/main/java/edu/sergradcapstone/groupseven/brewday/repositories/IngredientRepository.java
@@ -0,0 +1,9 @@
+package edu.sergradcapstone.groupseven.brewday.repositories;
+
+
+import edu.sergradcapstone.groupseven.brewday.model.Ingredient;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IngredientRepository extends CrudRepository<Ingredient, Long>{
+
+}
